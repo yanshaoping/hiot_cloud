a85be3b03ea48e3bc5ce481d14372034368133d2
1702-10-严少萍 <1956588696@qq.com>
2020-04-10 15:33:53 +0800

    添加MVP测试

--- app/src/main/java/com/huatec/hiot_cloud/MainActivity.java
+++ app/src/main/java/com/huatec/hiot_cloud/MainActivity.java
@@ -2,8 +2,14 @@ package com.huatec.hiot_cloud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("tag", "test");
+
     }
+
+
 }
